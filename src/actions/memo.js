b16d6597215efd44720b422be5ebbab12ca3a08d
4sizn <4sizn@naver.com>
2017-11-06 20:35:04 +0900

    MemoEdit / MemoRemove and Sec dashboard data

--- src/actions/memo.js
+++ src/actions/memo.js
@@ -4,7 +4,13 @@ import {
     MEMO_POST_FAILURE,
     MEMO_LIST,
     MEMO_LIST_SUCCESS,
-    MEMO_LIST_FAILURE
+    MEMO_LIST_FAILURE,
+    MEMO_EDIT,
+    MEMO_EDIT_SUCCESS,
+    MEMO_EDIT_FAILURE,
+    MEMO_REMOVE,
+    MEMO_REMOVE_SUCCESS,
+    MEMO_REMOVE_FAILURE
 } from './ActionTypes';
 import axios from 'axios';
 
@@ -94,5 +100,72 @@ export function memoListSuccess(data, isInitial, listType) {
 export function memoListFailure() {
     return {
         type: MEMO_LIST_FAILURE
+    };
+}
+
+/*MEMO EDIT*/
+export function memoEditRequest(id, index, contents) {
+    return (dispatch) => {
+        dispatch(memoEdit());
+
+        return axios.put('/api/memo/' + id, { contents })
+        .then((response) => {
+            dispatch(memoEditSuccess(index, response.data.memo));
+        }).catch((error) => {
+            dispatch(memoEditFailure(error.response.data.code));
+        });
+    };
+}
+
+export function memoEdit(){
+    return {
+        type : MEMO_EDIT
+    }
+}
+
+export function memoEditSuccess(){
+    return {
+        type : MEMO_EDIT_SUCCESS
+    }
+}
+
+export function memoEditFailure(){
+    return {
+        type : MEMO_EDIT_FAILURE
+    }
+}
+
+
+/*MEMO REMOVE*/
+export function memoRemoveRequest(id, index) {
+    return (dispatch) => {
+        dispatch(memoRemove());
+        
+        return axios.delete('/api/memo/' + id)
+        .then((response) => {
+            dispatch(memoRemoveSuccess(index));
+        }).catch((error) => {
+            dispatch(memoRemoveFailure(error.response.data.code));
+        });
+    };
+}
+
+export function memoRemove() {
+    return {
+        type: MEMO_REMOVE
+    };
+}
+
+export function memoRemoveSuccess(index) {
+    return {
+        type: MEMO_REMOVE_SUCCESS,
+        index
+    };
+}
+
+export function memoRemoveFailure(error) {
+    return {
+        type: MEMO_REMOVE_FAILURE,
+        error
     };
 }
--- src/components/MemoList.js
+++ src/components/MemoList.js
@@ -1,33 +1,54 @@
-import React, { Component } from "react";
-import Memo from "../components/Memo";
-import PropTypes from 'prop-types';
+import React from 'react';
+import { Memo } from 'components';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-class MemoList extends Component {
-  render() {
-    const maptoComponents = data => {
-      return data.map((memo, i) => {
-        return (
-          <Memo
-            data={memo}
-            ownership={memo.writer === this.props.currentUser}
-            key={memo._id}
-          />
+class MemoList extends React.Component {
+    render() {
+
+        const mapToComponents = data => {
+            return data.map((memo, i) => {
+                return (
+                    <Memo
+                        data={memo}
+                        ownership={ memo.writer===this.props.currentUser }
+                        key={memo._id}
+                        onEdit={this.props.onEdit}
+                        onRemove={this.props.onRemove}
+                        index={i}
+                    />
+                );
+            });
+        };
+
+        return(
+            <div>
+                <ReactCSSTransitionGroup
+                    transitionName="memo"
+                    transitionEnterTimeout={2000}
+                    transitionLeaveTimeout={1000}>
+                    {mapToComponents(this.props.data)}
+                </ReactCSSTransitionGroup>
+            </div>
         );
-      });
-    };
-
-    return <div>{maptoComponents(this.props.data)}</div>;
-  }
+    }
 }
 
 MemoList.propTypes = {
-  data: PropTypes.array,
-  currentUser: PropTypes.string
+    data: React.PropTypes.array,
+    currentUser: React.PropTypes.string,
+    onEdit: React.PropTypes.func,
+    onRemove: React.PropTypes.func
 };
 
 MemoList.defaultProps = {
-  data: [],
-  currentUser: ""
+    data: [],
+    currentUser: '',
+    onEdit: (id, index, contents) => {
+        console.error('onEdit not defined');
+    },
+    onRemove: (id, index) => {
+        console.error('onRemove not defined');
+    }
 };
 
 export default MemoList;
